5b5aea2e33a257c9a8aa1ff296b1848e22ccff58
Cleiber Dorneles <cleiberbomfim021@gmail.com>
2026-09-20 17:23:42 -0300

    Add files of challenges

---
+++ Technical Challenges/src/Challenge01/src/TechChallenge01.java
@@ -0,0 +1,9 @@
+package Challenge01.src;
+import Challenge01.src.domain.Challenge01;
+
+public class TechChallenge01 {
+    public static void main(String[] args) {
+        Challenge01 product = new Challenge01();
+
+    }
+}
