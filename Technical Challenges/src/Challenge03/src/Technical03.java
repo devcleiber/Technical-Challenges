66e1633e531622f3d09bd4135625923207d92ff8
Cleiber Dorneles <cleiberbomfim021@gmail.com>
2026-09-23 20:21:11 -0300

    edit repository files

--- Technical Challenges/src/Challenge03/src/Technical03.java
+++ Technical Challenges/src/Challenge03/src/Technical03.java
@@ -3,16 +3,16 @@ import Challenge03.src.domain.BankAccount;
 
 public class Technical03 {
     public static void main(String[] args) {
-        BankAccount bank = new BankAccount("01177", 0711076769021, 021000.0);
+        BankAccount bank = new BankAccount("01177", 9021, 021000.0);
         bank.display();
 
         bank.deposit(2500);
         bank.display();
 
-        bank.withdraw(3000);
+        bank.withdraw(43000);
         bank.display();
 
-        BankAccount banktwo = new BankAccount("qa01177", 0711076769021);
+        BankAccount banktwo = new BankAccount("qa01177", 7521);
         banktwo.deposit(0); // balance not = 0
         banktwo.display();
         banktwo.withdraw(280);//error withdraw>balance
@@ -21,12 +21,11 @@ public class Technical03 {
         BankAccount errorbank = new BankAccount("  ", 0, -1202.60);//error bank
         errorbank.display();
 
-    boolean hasBalance = errorbank.hasBalance(); // false < 0
-    System.out.println("Has balance: " + hasBalance);
+        boolean hasBalance = errorbank.hasBalance(); // false < 0
+        System.out.println("Has balance: " + hasBalance);
 
-    boolean hasBalance2 = bank.hasBalance(); // true > 0
-    System.out.println("Has balance: " + hasBalance2);
-
-
+        boolean hasBalance2 = bank.hasBalance(); // true > 0
+        System.out.println("Has balance: " + hasBalance2);
+    }
 }
 
